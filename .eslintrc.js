eac4f62e6c88ed4160851fcda3221889597e78e5
guicondee <guicondee@outlook.com>
2022-07-23 01:03:20 -0300

    disable prop-types rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,5 +23,7 @@ module.exports = {
     'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
     'import/prefer-default-export': 'off',
     'react/state-in-constructor': 'off',
+
+    'react/prop-types': 'off',
   },
 };
